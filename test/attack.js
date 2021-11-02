3430e681c9dd7a088c692288508ca3b07ecca360
talentdev2020 <polaris.center@outlook.com>
2021-11-02 04:24:29 -0700

    enable attack test case

--- test/attack.js
+++ test/attack.js
@@ -2,62 +2,62 @@ const { expect } = require("chai");
 const { ethers, waffle } = require("hardhat");
 const hre = require("hardhat");
 
-// describe("Reentrancy Attack success", function () {
-//   let bank;
-//   let attack;
-//   beforeEach(async function() {
-//     const Logger = await hre.ethers.getContractFactory("Logger");
-//     const logger = await Logger.deploy();
+describe("Reentrancy Attack success", function () {
+  let bank;
+  let attack;
+  beforeEach(async function() {
+    const Logger = await hre.ethers.getContractFactory("Logger");
+    const logger = await Logger.deploy();
 
-//     await logger.deployed();
+    await logger.deployed();
 
-//     const Bank = await hre.ethers.getContractFactory("Bank");
-//     bank = await Bank.deploy(logger.address);
+    const Bank = await hre.ethers.getContractFactory("Bank");
+    bank = await Bank.deploy(logger.address);
 
-//     await bank.deployed();
+    await bank.deployed();
 
-//     const Attack = await hre.ethers.getContractFactory("Attack");
-//     attack = await Attack.deploy(bank.address);
+    const Attack = await hre.ethers.getContractFactory("Attack");
+    attack = await Attack.deploy(bank.address);
 
-//     await attack.deployed();
+    await attack.deployed();
 
-//     // initial deposit
-//     const accounts = await hre.ethers.getSigners();
-//     await bank.deposit({
-//       from: accounts[0].address,
-//       value: ethers.utils.parseEther("15.0"),
-//     });
-//     await attack.deposit({
-//       from: accounts[0].address,
-//       value: ethers.utils.parseEther("8.0"),
-//     });
-//   })
+    // initial deposit
+    const accounts = await hre.ethers.getSigners();
+    await bank.deposit({
+      from: accounts[0].address,
+      value: ethers.utils.parseEther("15.0"),
+    });
+    await attack.deposit({
+      from: accounts[0].address,
+      value: ethers.utils.parseEther("8.0"),
+    });
+  })
 
-//   it("Should attack reentrancy success", async function () {
-//     const bankBalance = await bank.getBalance();
+  it("Should attack reentrancy success", async function () {
+    const bankBalance = await bank.getBalance();
 
-//     const attackBalance = await attack.getBalance();
-//     try {
-//       await attack.attack();
-//     } catch (err) {
-// console.log(err)
-//     }
-//     const attackedBalance = await attack.getBalance();
-//     expect(parseFloat(attackedBalance.toString())).to.equal(parseFloat(bankBalance.toString()) + parseFloat(attackBalance.toString()));
+    const attackBalance = await attack.getBalance();
+    try {
+      await attack.attack();
+    } catch (err) {
+console.log(err)
+    }
+    const attackedBalance = await attack.getBalance();
+    expect(parseFloat(attackedBalance.toString())).to.equal(parseFloat(bankBalance.toString()) + parseFloat(attackBalance.toString()));
  
-//   });
+  });
 
-//   it("Should bank balance 0", async function () {
-//     try {
-//       await attack.attack();
-//       const initialBalance = await bank.getBalance();
+  it("Should bank balance 0", async function () {
+    try {
+      await attack.attack();
+      const initialBalance = await bank.getBalance();
 
-//       expect(initialBalance.toString()).to.equal("0");
-//     } catch(err) {
+      expect(initialBalance.toString()).to.equal("0");
+    } catch(err) {
 
-//     }
-//   });
-// });
+    }
+  });
+});
 
 describe("Trap Hacker", function () {
   let bank;
